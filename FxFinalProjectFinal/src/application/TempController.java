126c33d0a6fcfdac0101d1c279fcde4ac5690501
Md.Akteruzzaman Dipto <akteruzzamandipto257@gmail.com>
2022-10-09 18:39:15 +0600

    extracted from zip

---
+++ FxFinalProjectFinal/src/application/TempController.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import banking.*;
+
+public class TempController {
+	public ListView<String> lv;
+	
+	public void display() {
+//		lv.getItems().clear();
+		ObservableList<BankAccount> bList = FXCollections.observableArrayList(Main.bank.getAccounts());
+		for(int i=0; i < bList.size();i++) {
+			lv.getItems().addAll(bList.get(i).toString());
+		}
+	}
+	
+	public void refresh(ActionEvent event) {
+		lv.getItems().clear();
+		display();
+	}
+}
---
+++ FxFinalProjectFinal/src/application/TransferController.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.io.IOException;
+
+import banking.InSufficientBalanceException;
+import banking.InvalidAccountException;
+import banking.MaxWithdawException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class TransferController {
+
+	Parent root;
+	Stage stage;
+	Scene scene;
+	
+	@FXML
+	TextField fanTF;
+	
+	@FXML
+	TextField tanTF;
+	
+	@FXML
+	TextField amtTF;
+	@FXML
+	Label lbl;
+	
+	public void next(ActionEvent event) {
+		String fAccNum = fanTF.getText();
+		String tAccNum = tanTF.getText();
+		double amount = Double.parseDouble(amtTF.getText());
+		
+		try {
+			Main.bank.transfer(fAccNum, tAccNum, amount);
+			lbl.setText("Transfer successful!");
+		} catch (InvalidAccountException e) {
+			// TODO Auto-generated catch block
+			lbl.setText("Sorry account not found");
+//			e.printStackTrace();
+		} catch (InSufficientBalanceException e) {
+			// TODO Auto-generated catch block
+			lbl.setText("Sorry you don't have enough balance to transfer");
+//			e.printStackTrace();
+		} catch (MaxWithdawException e) {
+			// TODO Auto-generated catch block
+			lbl.setText("Sorry this amount is more than your maximum withdraw limit");
+//			e.printStackTrace();
+		}
+		Main.bank.saveData();
+		fanTF.clear();
+		tanTF.clear();
+	}
+	
+	public void back(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("EmployeeMenu.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
